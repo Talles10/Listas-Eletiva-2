1265afb81ed3407b4b5648ba1b12771618866f2b
Talles Augusto Passos Siscoutto <tallessiscoutto09@gmail.com>
2025-02-28 21:51:45 -0300

    .

--- primeiralista/app/Http/Controllers/ExerciciosController.php
+++ primeiralista/app/Http/Controllers/ExerciciosController.php
@@ -9,11 +9,9 @@ class ExerciciosController extends Controller
     public function abrirFormExer1(){
         return view('lista.ex1');
     }
-
     public function respostaExer1(Request $request){
     $valor1 = intval($request -> input('valor1'));
     $valor2 = intval($request -> input('valor2'));
     $soma = $valor1 + $valor2;
     return view('exer1', compact('soma'));
-}
-}
+}}
